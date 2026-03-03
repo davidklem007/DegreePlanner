c88af3f5d40d262a8d35d6ca3d2ffa5a81744714
daklem <daklem@iu.edu>
2026-03-03 16:32:02 -0500

    starter code

---
+++ src/main/java/com/example/DegreePlanner/DegreePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.DegreePlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DegreePlanner/DegreePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DegreePlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
